92d2119982c71f76e3398048ca35733b5f17e128
rthaqori <ramanthaqori@gmail.com>
2024-08-27 01:10:33 +0545

    commit

--- src/components/Sections/ProjectsSection.jsx
+++ src/components/Sections/ProjectsSection.jsx
@@ -8,7 +8,7 @@ const ProjectsSection = () => {
     {
       title: "ShoeEcommerce",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam consectetur recusandae aut soluta quasi, rerum nesciunt quas temporibus asperiores eius.",
+        "Ecommerce Shoe Site built with React.js and Tailwind CSS, allowing users to browse various shoe products, filter by gender, category, size, price, and colour, add their favourite items to a favourites list and add the products they want to buy to their bag.",
       image: "./assets/shoe.png",
       GitHub: "https://github.com/rthaqori/ecommerce_shoe_project",
       Live: "https://ecommerce-shoe-project.vercel.app/",
@@ -16,7 +16,7 @@ const ProjectsSection = () => {
     {
       title: "ClothesEcommerce",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam consectetur recusandae aut soluta quasi, rerum nesciunt quas temporibus asperiores eius.",
+        "Ecommerce Clothes Site developed with React.js and Tailwind CSS, featuring product browsing, filtering by gender, category, size, price, and colour, and a favourites list. Additionally, it integrates Google Firebase for user creation, login authentication, and product management in the database.",
       image: "./assets/clothes.png",
       GitHub: "https://github.com/rthaqori/ecommerce_clothes_project",
       Live: "https://ecommerce-clothes-project.vercel.app/",
@@ -24,7 +24,7 @@ const ProjectsSection = () => {
     {
       title: "Wordle Game",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam consectetur recusandae aut soluta quasi, rerum nesciunt quas temporibus asperiores eius.",
+        "A Wordle Game built with React.js and Tailwind CSS, designed for players to guess words in order to win. The game offers a responsive experience across all devices and includes a virtual keyboard specifically tailored for mobile users, ensuring smooth and intuitive word entry. With a clean interface and engaging gameplay, it's both challenging and accessible, making it enjoyable for players of all skill levels.",
       image: "./assets/wordle.png",
       GitHub: "https://github.com/rthaqori/wordle_project",
       Live: "https://project-wordle-omega.vercel.app/",
@@ -32,7 +32,7 @@ const ProjectsSection = () => {
     {
       title: "Amazon Demo",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam consectetur recusandae aut soluta quasi, rerum nesciunt quas temporibus asperiores eius.",
+        "An Amazon UI clone created using HTML, vanilla CSS, and vanilla JavaScript, designed to replicate the look and feel of the Amazon website. This project recreates the core layout and interactions to provide a similar shopping experience, showcasing the ability to build responsive and functional web pages without using frameworks.",
       image: "./assets/amazon.png",
       GitHub: "https://github.com/rthaqori/Amazon",
       Live: "https://amazoncopy-ten.vercel.app/",
